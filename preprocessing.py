4c63368f9129b0dad0f9d681ce7f87ad0740579e
蒋硕 <111181768+JSLiam94@users.noreply.github.com>
2024-09-26 18:41:39 +0800

    Update preprocessing.py
    match BRATS2024 GLI

--- preprocessing.py
+++ preprocessing.py
@@ -5,56 +5,56 @@ import tensorflow as tf
 import shutil
 
 def DeepMedicFormat_crop():
-    raw_data_pth = ''
-    save_data_pth = ''
-
+    raw_data_pth = '/home/zhushenghao/data/JS/Brats2024/training_data1_v2'
+    save_data_pth = '/home/zhushenghao/data/JS/Robust-Mseg-main/brats_2024'
     data_fds = os.listdir(raw_data_pth)
     data_fds.sort()
     for data_fd in data_fds:
         img_fds = os.listdir(raw_data_pth+'/'+data_fd)
         img_fds.sort()
         for img_fd in img_fds:
-            print img_fd
-            if not os.path.exists(save_data_pth+'/'+data_fd+'_'+img_fd):
-                os.makedirs(save_data_pth+'/'+data_fd+'_'+img_fd)
+
+            #print(img_fd)
+            if not os.path.exists(save_data_pth+'/'+data_fd):
+                os.makedirs(save_data_pth+'/'+data_fd)
             modality_fds = os.listdir(raw_data_pth+'/'+data_fd+'/'+img_fd)
+            #print(raw_data_pth + '/' + data_fd + '/' + img_fd)
+            #print(modality_fds)
             modality_fds.sort()
             modality_fds[1], modality_fds[0] = modality_fds[0], modality_fds[1]
 
             brainmask_arr_list = []
             for modality_fd in modality_fds:
-                if 'MR' in modality_fd:
-                    modality_name = modality_fd.split('.')[4].split('_')[-1]+'_subtrMeanDivStd'
-                else:
-                    modality_name = 'OTMultiClass'
-                if os.path.exists(raw_data_pth + '/' + data_fd + '/' + img_fd + '/' + modality_fd + '/' + modality_fd + '.mha'):
-                    sitk.WriteImage(sitk.ReadImage(raw_data_pth + '/' + data_fd + '/' + img_fd + '/' + modality_fd + '/' + modality_fd + '.mha'),
-                                    save_data_pth + '/'+data_fd+'_'+img_fd + '/' + modality_name + '.nii.gz')
 
-                if 'MR' in modality_fd:
-                    image_arr, image_header = medio.load(save_data_pth + '/' +data_fd+'_'+img_fd + '/' + modality_name + '.nii.gz')
+
+                if 'seg' not in modality_fd:
+                    print("load: "+raw_data_pth + '/' +data_fd+'/'+img_fd + '/' + data_fd+'-'+modality_fd[-10:-7] + '.nii.gz')
+                    image_arr, image_header = medio.load(raw_data_pth + '/' +data_fd+'/'+img_fd + '/' + data_fd+'-'+modality_fd[-10:-7] + '.nii.gz')
                     brainmask_arr = image_arr.copy()
                     brainmask_arr[brainmask_arr > 0] = 1
                     brainmask_arr_list.append(brainmask_arr)
 
             brainmask_arr = brainmask_arr_list[0]
-            for m in xrange(1,len(brainmask_arr_list)):
+            for m in range(1,len(brainmask_arr_list)):
                 brainmask_arr = brainmask_arr+brainmask_arr_list[m]
             brainmask_arr[brainmask_arr>0]=1
             # brainmask_arr[brainmask_arr <4] = 0
+            print("save: "+save_data_pth + '/'+data_fd + '/'+ data_fd+'-brainmask.nii.gz')
             medio.save(brainmask_arr,
-                       save_data_pth + '/'+data_fd+'_'+img_fd + '/brainmask.nii.gz',
+                       save_data_pth + '/'+data_fd +'/'+data_fd+'-brainmask.nii.gz',
                        image_header)
 
-            brainmask_arr, brainmask_header = medio.load(save_data_pth + '/' + data_fd + '_' + img_fd + '/brainmask.nii.gz')
+            brainmask_arr, brainmask_header = medio.load(save_data_pth + '/' +data_fd +'/'+data_fd  +'-brainmask.nii.gz')
             roi_ind = np.where(brainmask_arr > 0)
             roi_bbx = [roi_ind[0].min(), roi_ind[0].max(), roi_ind[1].min(), roi_ind[1].max(), roi_ind[2].min(), roi_ind[2].max()]
             for modality_fd in modality_fds:
-                if 'MR' in modality_fd:
-                    modality_name = modality_fd.split('.')[4].split('_')[-1]+'_subtrMeanDivStd'
-                    image_arr, image_header = medio.load(save_data_pth + '/'+data_fd+'_'+img_fd + '/' + modality_name + '.nii.gz')
+                if 'seg' not in modality_fd:
+
+                    image_arr, image_header = medio.load(raw_data_pth + '/' +data_fd+'/'+img_fd + '/' + data_fd+ '-'+modality_fd[-10:-7]+ '.nii.gz')
+
 
                     roi_arr = image_arr[brainmask_arr>0]
+
                     lower_limit = np.percentile(roi_arr, 1)
                     upper_limit = np.percentile(roi_arr, 99)
                     roi_arr = roi_arr[roi_arr>lower_limit]
@@ -66,31 +66,33 @@ def DeepMedicFormat_crop():
                     image_arr_crop = image_arr[roi_bbx[0]:roi_bbx[1]+1, roi_bbx[2]:roi_bbx[3]+1, roi_bbx[4]:roi_bbx[5]+1]
 
                     medio.save(image_arr_crop,
-                               save_data_pth + '/'+data_fd+'_'+img_fd + '/' + modality_name + '.nii.gz',
-                               image_header)
+                               save_data_pth + '/' +data_fd +  '/' + data_fd+'-'+modality_fd[-10:-7]+'-subtrMeanDivStd' + '.nii.gz')
+                    print("save: " + save_data_pth + '/' +data_fd +  '/' + data_fd+'-'+modality_fd[-10:-7]+'-subtrMeanDivStd' + '.nii.gz')
                 else:
-                    modality_name = 'OTMultiClass'
-                    image_arr, image_header = medio.load(save_data_pth + '/' + data_fd + '_' + img_fd + '/' + modality_name + '.nii.gz')
+                    #modality_name = 'OTMultiClass'
+                    image_arr, image_header = medio.load(raw_data_pth + '/' +data_fd+'/'+img_fd + '/' + data_fd+'-'+ modality_fd[-10:-7] + '.nii.gz')
                     image_arr_crop = image_arr[roi_bbx[0]:roi_bbx[1] + 1, roi_bbx[2]:roi_bbx[3] + 1, roi_bbx[4]:roi_bbx[5] + 1]
                     medio.save(image_arr_crop,
-                               save_data_pth + '/' + data_fd + '_' + img_fd + '/' + modality_name + '.nii.gz',
+                               save_data_pth + '/' + data_fd  + '/' +data_fd+'-'+modality_fd[-10:-7] + '.nii.gz',
                                image_header)
+                    print("save: " + save_data_pth + '/' + data_fd  + '/'+ data_fd+'-'+modality_fd[-10:-7] + '.nii.gz')
 
             brainmask_arr_crop = brainmask_arr[roi_bbx[0]:roi_bbx[1]+1, roi_bbx[2]:roi_bbx[3]+1, roi_bbx[4]:roi_bbx[5]+1]
             medio.save(brainmask_arr_crop,
-                       save_data_pth + '/' + data_fd + '_' + img_fd + '/brainmask.nii.gz',
+                       save_data_pth + '/' +data_fd  + '/' + data_fd+'-brainmask' + '.nii.gz',
                        brainmask_header)
+            print("save: " + save_data_pth + '/' +data_fd  + '/' + data_fd+'-brainmask' + '.nii.gz')
 
 
-def nii2tfrecord():
+def nii2tfrecord(txt):
     os.environ['CUDA_VISIBLE_DEVICES'] = '0'
-    raw_data_pth = ''
-    save_data_pth = ''
 
+    raw_data_pth = '/home/zhushenghao/data/JS/Robust-Mseg-main/brats_2024'
+    save_data_pth = '/home/zhushenghao/data/JS/Robust-Mseg-main/brats_all'
     pid_all = os.listdir(raw_data_pth)
     pid_all.sort()
 
-    with open('../Data/train.txt', 'r') as fp:
+    with open(txt, 'r') as fp:
         rows = fp.readlines()
 
     pid_all = [row[:-1] for row in rows]
@@ -124,9 +126,15 @@ def nii2tfrecord():
             example = tf.train.Example(features=tf.train.Features(feature=feature))
             writer.write(example.SerializeToString())
             writer.close()
-            print (pid, modality, cnt)
+            print(pid, modality, cnt)
 
-
+def nii2tfrecord_all(txt_train,txt_test):
+    nii2tfrecord(txt_train)
+    nii2tfrecord(txt_test)
 
 if __name__=='__main__':
-    nii2tfrecord()
+
+    txt_train='/home/zhushenghao/data/JS/Robust-Mseg-main/datalist/train.txt'
+    txt_test='/home/zhushenghao/data/JS/Robust-Mseg-main/datalist/test.txt'
+    #DeepMedicFormat_crop()
+    nii2tfrecord_all(txt_train,txt_test)
